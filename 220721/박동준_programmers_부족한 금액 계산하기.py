7d711d11571aeb00e5fcdea01dfc2005eeb505dc
DONG-8 <junzzamg9@gmail.com>
2022-07-21 21:26:07 +0900

    add : 220721 programmers_부족한 금액 계산하기 Solved

--- 220721/박동준_programmers_부족한 금액 계산하기.py
+++ 220721/박동준_programmers_부족한 금액 계산하기.py
@@ -1,11 +1,14 @@
 def solution(price, money, count):
     answer = -1
+    # 돌려서 뺀다
     for i in range(1,count+1):
         money -= i*price
     
+    # 음수면 해당값 양수로 바꾸고 리턴
     if money < 0:
         answer = abs(money)
+    # 돈이 안부족하면 0 리턴
     else:
         answer = 0
-    
+
     return answer
