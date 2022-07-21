1201a8401e6191380744f19c88deb1de14348b15
ksh103 <jayy_z@naver.com>
2022-07-21 21:12:37 +0900

    add : 220721 programmers_부족한 금액 계산하기 Solved

--- 220721/강소현_programmers_부족한 금액 계산하기.java
+++ 220721/강소현_programmers_부족한 금액 계산하기.java
@@ -1,7 +1,6 @@
 class Solution {
     public long solution(int price, int money, int count) {
         long total = 0;
-        
         for(int i = 1; i <= count; i++) {
             total += i * price;
         }
