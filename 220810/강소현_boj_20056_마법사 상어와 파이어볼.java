bb9348433b03357d105b89e73116ebe09ff0de57
ksh103 <jayy_z@naver.com>
2022-08-16 20:05:16 +0900

    add : 220810 boj_20056 Solved

--- 220810/강소현_boj_20056_마법사 상어와 파이어볼.java
+++ 220810/강소현_boj_20056_마법사 상어와 파이어볼.java
@@ -1,19 +1,17 @@
-package boj;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /* boj 20056번 마법사 상어와 파이어볼 */
-public class boj_20056 {
+public class Main {
 
 	static int N, M, K;
 	static ArrayList<FireBall>[][] fireBalls;
-	static boolean[][] visited;
 	
-	static int[] dr = {-1, 1, 0, 0, 1, -1, 1, -1};
-	static int[] dc = {0, 0, -1, 1, 1, 1, -1, -1};
+	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -21,13 +19,13 @@ public class boj_20056 {
 		
 		N = Integer.parseInt(st.nextToken()); // 배열 크기
 		M = Integer.parseInt(st.nextToken()); // 파이어볼 개수
-		K = Integer.parseInt(st.nextToken()); // 이동 명렁 횟수
+		K = Integer.parseInt(st.nextToken()); // 이동 명령 횟수
 		
-		visited = new boolean[N][N];
+		fireBalls = new ArrayList[N][N];
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				fireBalls[i][j] = new ArrayList<>();
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				fireBalls[r][c] = new ArrayList<>();
 			}
 		}
 
@@ -37,61 +35,128 @@ public class boj_20056 {
 			int r = Integer.parseInt(st.nextToken()) - 1; // 파이어볼 위치
 			int c = Integer.parseInt(st.nextToken()) - 1; // 파이어볼 위치
 			int m = Integer.parseInt(st.nextToken()); // 질량
-			int d = Integer.parseInt(st.nextToken()); // 방향
 			int s = Integer.parseInt(st.nextToken()); // 속력
+			int d = Integer.parseInt(st.nextToken()); // 방향
 			
-			
-			fireBalls[r][c].add(new FireBall(r, c, m, d, s));
+			fireBalls[r][c].add(new FireBall(m, s, d));
 		}
 		
 		while(K --> 0) {
+			// 파이어볼 이동
 			move();
+			
+			// 파이어볼 나누기
+			split();
 		}
+		
+		System.out.println(sum());
+
 	}
 	
 	static void move() {
+		ArrayList<FireBall>[][] copy = new ArrayList[N][N];
 		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				copy[r][c] = new ArrayList<>();
+			}
+		}
+ 		
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
 				
-				for(int k = 0; k < fireBalls[i][j].size(); k++) {
-					FireBall f = fireBalls[i][j].get(k);
-					
-					int r = f.r;
-					int c = f.c;
-					int m = f.m;
-					int d = f.d;
-					int s = f.s;
-					
-					if(visited[r][c]) continue;
-					
-					visited[r][c] = true;
-					
+				if(fireBalls[r][c].size() == 0) continue;
+				
+				for(FireBall fb : fireBalls[r][c]) {
+					int m = fb.m;
+					int s = fb.s;
+					int d = fb.d;
+				
 					// 모든 파이어볼이 자신의 방향 d로 속력 s칸 만큼 이동
-					int nr = (r + dr[d] * s);
-					int nc = (c + dc[d] * s);
+					// 1번 행은 N번과 연결되어 있고, 1번 열은 N번 열과 연결되어 있다. -> 순환을 의미
+					int nr = (r + dr[d] * s) % N;
+					int nc = (c + dc[d] * s) % N;
+					
+					if(nr < 0) nr += N;
+					if(nc < 0) nc += N;
+					
+					copy[nr][nc].add(new FireBall(m, s, d));
+				}
+			}
+		}
+		fireBalls = copy;
+	}
+	
+	static void split() {
+		
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				
+				// 파이어볼이 2개 이상일 때 
+				if(fireBalls[r][c].size() >= 2) {
+					
+					int mTotal = 0; // 질량 합 초기화
+					int sTotal = 0; // 속력 합 초기화
+					
+					boolean even = true;
+					boolean odd = true;
 					
 					
-					fireBalls[nr][nc].add(new FireBall(nr, nc, m, d, s));
+					for(FireBall fb : fireBalls[r][c]) {
+						mTotal += fb.m; // 질량 합
+						sTotal += fb.s; // 속력 합
+						
+						// 짝수이면
+						if(fb.d % 2 == 0) even = false;
+						else odd = false;
+						
+					}
 					
+					mTotal /= 5; // 질량은 (합쳐진 파이어볼 질량의 합)/5
+					sTotal /= fireBalls[r][c].size(); // 속력은 (합쳐진 파이어볼 속력의 합)/(합쳐진 파이어볼의 개수)
+
+					fireBalls[r][c].clear();
+					
+					// 질량의 합이 0이면 파이어볼 소멸
+					if(mTotal > 0) {
+						for(int d = 0; d < 4; d++) {
+							// 모두 홀수이거나 모두 짝수 (0, 2, 4, 6)
+							if(even || odd) {
+								fireBalls[r][c].add(new FireBall(mTotal, sTotal, d * 2));
+							}
+							// (1, 3, 5, 7)
+							else { 
+								fireBalls[r][c].add(new FireBall(mTotal, sTotal, d * 2 + 1));
+							}
+						}
+					}
 				}
 			}
 		}
 	}
+
 	
-	static void fire() {
-		
+	static int sum() {
+		int sum = 0;
+		for(int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				
+				for(FireBall fb : fireBalls[r][c]) {
+					sum += fb.m;
+				}
+			}
+		}		
+		return sum;
 	}
 	
+	
 	static class FireBall {
-		int r, c, m, d, s;
+		int m, s, d;
 		
-		public FireBall(int r, int c, int m, int d, int s) {
-			this.r = r;
-			this.c = c;
+		public FireBall(int m, int s, int d) {
 			this.m = m;
-			this.d = d;
 			this.s = s;
+			this.d = d;
 		}
 	}
 }
