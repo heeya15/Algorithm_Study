33ec0a307b3f21edc237eaeb455e05f0c8c40e42
DONG-8 <junzzamg9@gmail.com>
2022-09-07 22:49:09 +0900

    add : 220907 boj_13397 Solved

---
+++ 220907/박동준_boj_13397_구간 나누기2.py
@@ -0,0 +1,27 @@
+
+n,m=map(int,input().strip().split())
+arr=list(map(int,input().strip().split()))
+
+def gugan(mid):
+    max_x=min_x=arr[0]
+    cnt=1
+    for i in range(1,n):
+        max_x=max(max_x,arr[i])
+        min_x=min(min_x,arr[i])
+        if max_x - min_x > mid:
+            cnt +=1
+            max_x=arr[i]
+            min_x=arr[i]
+    return cnt
+
+start, end = 0, max(arr)
+result=0
+while start<=end:
+    mid=(start+end)//2
+    if gugan(mid)<=m:
+        end = mid-1
+        result=mid
+    else:
+        start = mid+1
+
+print(result)
