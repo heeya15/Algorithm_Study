0fba4a7ee30ae8eaa98581f3b80c9cc0f413908f
redniche <redniche@naver.com>
2022-09-22 20:55:53 +0900

    fix : 220922 programmers_코딩 테스트 공부 Solved
    [풀이과정]
    DP로 푼다.
    1. 알고력과 코딩력을 각각 행과 열로 두고 각 원소 값은 해당 알고력과 코딩력에 도달하는데 걸리는 시간을 적는다.
    2. 알고력+1일 때와 아닐 때를 비교해서 작은 값으로 바꾼다
    3. 코딩력+1일 때와 아닐 때를 비교해서 작은 값으로 바꾼다
    4. 문제를 풀 수 있다면 풀었을 때와 아닐 때를 비교해서 작은 값으로 바꾼다.
    5. 모든 i행과 j열에 대하여 2~4를 반복한다.
    
    처음엔 2와 3을 행과 열의 인덱스로 계산할 수 있다는 생각을 하고 있었음.

--- 220914/장예찬_programmers_코딩 테스트 공부.py
+++ 220914/장예찬_programmers_코딩 테스트 공부.py
@@ -1,48 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118668
 # 예상 풀이: DP
-# 베스트 풀이
-
-'''
-            코딩력
-            
-                0   1   2   3   4   5   6   7   8   9   10  11  12
-알고력
-0               0   1   2   3   4   5   6   7   8   9   10  11  12
-1               1   2   3   4   5   6   7   8   9   10  11  12  13
-2               2   3   4   5   6   7   8   9   10  11  12  13  14
-3               3
-4               4
-5               5
-6               6
-7               7
-'''
+# 베스트 풀이: DP, Dijkstra
 
 class Solution:
     def __init__(self, alp, cop, problems):
-        self.alp, self.cop = alp, cop
         problemsTransform = list(zip(*problems))
         self.problems = problems
         self.alpMax, self.copMax = max(problemsTransform[0]), max(problemsTransform[1])
-        self.alpCopMax = max(self.alpMax, self.copMax)
-        self.alpLen, self.copLen = self.alpMax - self.alp, self.copMax-self.cop
-
-        self.dpBoard =  [[0 for _ in range(self.copLen+1)] for _ in range(self.alpLen+1)]
+        self.alp, self.cop = min(self.alpMax, alp), min(self.copMax, cop)
+        self.alpLen, self.copLen = self.alpMax+2, self.copMax+2
+        self.dpBoard =  [[151 for _ in range(self.copLen)] for _ in range(self.alpLen)]
+        self.dpBoard[self.alp][self.cop] = 0
 
     def solve(self):
-        for alp_req, cop_req, alp_rwd, cop_rwd, cost in self.problems:
-            alp_req, cop_req = alp_req - self.alp, cop_req - self.cop
-            cnt = 1
-            while True:
-                nx, ny = alp_req+cnt*alp_rwd, cop_req+cnt*cop_rwd
-                if nx > self.alpLen or ny > self.copLen:
-                    break
-                temp = self.dpBoard[alp_req][cop_req] + alp_req+cop_req + cost*cnt
-                if temp < nx+ny + self.dpBoard[nx][ny]:
-                    self.dpBoard[nx][ny] -= (nx+ny + self.dpBoard[nx][ny])-temp
-                cnt += 1 
-        for s in self.dpBoard:
-            print(s)
-        return self.alpLen + self.copLen + self.dpBoard[self.alpLen][self.copLen]
+        for i in range(self.alp, self.alpMax+1):
+            for j in range(self.cop, self.copMax+1):
+                self.dpBoard[i+1][j] = min(self.dpBoard[i+1][j], self.dpBoard[i][j]+1)
+                self.dpBoard[i][j+1] = min(self.dpBoard[i][j+1], self.dpBoard[i][j]+1)
+                for alp_req, cop_req, alp_rwd, cop_rwd, cost in self.problems:
+                    if i >= alp_req and j >= cop_req:
+                        ni, nj = min(self.alpMax, i + alp_rwd), min(self.copMax,j + cop_rwd)
+                        self.dpBoard[ni][nj] = min(self.dpBoard[ni][nj], self.dpBoard[i][j]+cost)
+        return self.dpBoard[self.alpMax][self.copMax]
 
 def solution(alp, cop, problems):
     return Solution(alp, cop, problems).solve()
