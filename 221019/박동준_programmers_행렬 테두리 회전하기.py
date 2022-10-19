1bb24cc35944fe8ada0f37a29cc7f1eddf58048a
DONG-8 <junzzamg9@gmail.com>
2022-10-19 21:36:46 +0900

    add : 221019 programmers_행렬 테두리 회전하기 Solved

---
+++ 221019/박동준_programmers_행렬 테두리 회전하기.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def solution(rows, columns, queries):
+    answer = []
+    
+    table =  []
+    count = 1
+    for i in range(rows):
+        column = []
+        for j in range(columns):
+            column.append(count)
+            count += 1
+        table.append(column)
+    
+
+    for query in queries:
+        query = [x-1 for x in query] # 0부터 시작하는 인덱스에 맞춰 1씩 빼줌
+        tmp = table[query[0]][query[1]] # 왼쪽 위 값 저장
+        small = tmp
+        
+        # left
+        for i in range(query[0]+1, query[2]+1):
+            table[i-1][query[1]] = table[i][query[1]]
+            small = min(small, table[i][query[1]])
+        # bottom
+        for i in range(query[1]+1, query[3]+1):
+            table[query[2]][i-1] = table[query[2]][i]
+            small = min(small, table[query[2]][i])
+        # right
+        for i in range(query[2]-1, query[0]-1, -1):
+            table[i+1][query[3]] = table[i][query[3]]
+            small = min(small, table[i][query[3]])
+        # top
+        for i in range(query[3]-1, query[1]-1, -1):
+            table[query[0]][i+1] = table[query[0]][i]
+            small = min(small, table[query[0]][i])
+        table[query[0]][query[1]+1] = tmp
+        
+        answer.append(small)
+            
+    return answer
