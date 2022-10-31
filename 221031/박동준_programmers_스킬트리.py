2717a35dcb21f6b116537816ddfbf51ac2f76eb7
DONG-8 <junzzamg9@gmail.com>
2022-10-31 21:38:06 +0900

    add : 221031 programmers_스킬트리 Solved

---
+++ 221031/박동준_programmers_스킬트리.py
@@ -0,0 +1,16 @@
+def solution(skill, skill_trees):
+    answer = -1
+    arr =  []
+    for i in skill_trees:
+        word = ""
+        for j in i:
+            if j in skill:
+                word += j
+        arr.append(word)
+    count = 0
+    for k in arr:
+        if k in skill and skill[:len(k)] == k:
+            count += 1
+            
+    print(count)
+    return count
