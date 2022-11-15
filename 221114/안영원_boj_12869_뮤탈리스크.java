43040760b500f072817bfb61b9107c87e655d654
Youngwon An <kublaihkan@gmail.com>
2022-11-15 16:04:22 +0900

    add : 221114 boj_12869 Solved

--- 221114/안영원_boj_12869_뮤탈리스크.java
+++ 221114/안영원_boj_12869_뮤탈리스크.java
@@ -8,23 +8,26 @@ public class Main {
 	static ArrayList<int[]> wayList;
 	static int[] result;
 	static boolean[] visited;
+	static boolean[][][] check;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 
+		result = new int[n];
+		visited = new boolean[n];
+		wayList = new ArrayList<>();
+
 		int[] scv = new int[n];
+		check = new boolean[61][61][61];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			scv[i] = Integer.parseInt(st.nextToken());
 		}
 
 		answer = Integer.MAX_VALUE;
-		result = new int[n];
-		visited = new boolean[n];
-		wayList = new ArrayList<>();
 		findWay(n, 0); // n개일 때 공격가능한 순서(조합) 찾기
-		System.out.println(attack(scv));
+		System.out.println(attack(scv)); // 찾은 조합으로 최소 횟수 찾기
 	}
 
 	static int attack(int[] scv) {
@@ -33,20 +36,40 @@ public class Main {
 
 		while (!q.isEmpty()) {
 			Node node = q.poll();
+			
+			// 전부 파괴되었다면 횟수를 리턴함(BFS라서 먼저 찾은 값이 최소값)
 			if (checkDestroy(node.scv)) return node.cnt;
 
 			for (int[] way : wayList) {
 				int[] curScv = node.scv.clone();
 				for (int i = 0; i < n; i++) {
 					curScv[way[i]] -= damage[i];
+					if (curScv[way[i]] < 0) curScv[way[i]] = 0;
 				}
-				q.offer(new Node(curScv, node.cnt + 1));
+				// 남은 HP가 이미 탐색해본 것이라면 추가로 탐색하지 않음
+				// n이 3보다 작으면 뒤에 0을 추가해줌
+				if (n == 3) {
+					if (!check[curScv[0]][curScv[1]][curScv[2]]) {
+						check[curScv[0]][curScv[1]][curScv[2]] = true;
+						q.offer(new Node(curScv, node.cnt + 1));
+					}
+				} else if (n == 2) {
+					if (!check[curScv[0]][curScv[1]][0]) {
+						check[curScv[0]][curScv[1]][0] = true;
+						q.offer(new Node(curScv, node.cnt + 1));
+					}
+				} else {
+					if (!check[curScv[0]][0][0]) {
+						check[curScv[0]][0][0] = true;
+						q.offer(new Node(curScv, node.cnt + 1));
+					}
+				}
 			}
 		}
 
 		return 0;
 	}
-	
+
 	static boolean checkDestroy(int[] scv) {
 		boolean isDestroy = true;
 
