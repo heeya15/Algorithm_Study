94d477c51404729250b728d75f0f0243bf92dd90
장예찬 <redniche@naver.com>
2022-11-16 21:44:26 +0900

    fix : 221116 programmers_연속 부분 수열 합의 개수 Solved
    <풀이과정>
    1. 집합 자료구조로 푼다.
    2. 합을 구한 인덱스를 저장하는 [idx, 합] 를 원소로 갖는 리스트를 만든다.
    3. 인덱스를 1씩 증가시키면서 기존의 합에서 바로 옆 인덱스의 원소를 더하면 길이가 늘어난 합이다.
    4. 이를 elements의 길이 - 1만큼 반복하면 모든 경우가 구해진다.
    
    시간복잡도가 개선됨!
    4. 길이 출력

--- 221116/장예찬_programmers_연속 부분 수열 합의 개수.py
+++ 221116/장예찬_programmers_연속 부분 수열 합의 개수.py
@@ -1,16 +1,19 @@
-# 
-# 예상 알고리즘:
-# 베스트 알고리즘:
+# https://school.programmers.co.kr/learn/courses/30/lessons/131701
+# 예상 알고리즘: 구현
+# 베스트 알고리즘: 구현
 
 def solution(elements):
-    elementsTemp = list(elements) + list(elements) #원형 수열이므로 두 번 붙여줌
+    elementsTemp = [[idx, d] for idx, d in enumerate(elements)] # 초기 길이가 1인 부분수열의 인덱스와 값들을 저장
     answerSet = set(elements)
 
-    print(elements)
-    # 길이가 1씩 늘어나는 원형 수열의 부분 수열의 합을 구함
-    for i in range(1, len(elements)+1):
-        for j in range(len(elements)):
-            answerSet.add(sum(elementsTemp[j:j+i]))
+    length = len(elements)
+    for _ in range(len(elements) - 1): # 부분수열의 길이가 1인 경우는 이미 구했으므로 length - 1번 반복
+        for temp in elementsTemp:
+            # 원형으로 인덱스를 1 증가시킴
+            temp[0] = (temp[0] + 1) % length
+            temp[1] += elements[temp[0]] # 값에 다음 인덱스의 값을 더함
+            answerSet.add(temp[1]) # 부분수열의 합을 저장
+
     return len(answerSet)
 
 # 테스트
