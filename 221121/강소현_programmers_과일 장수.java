23188a170a6afdcd44fa42851c6477e3828d9aad
ksh103 <jayy_z@naver.com>
2022-11-22 19:45:20 +0900

    add : 221121 programmers_과일 장수 Solved

--- 221121/강소현_programmers_과일 장수.java
+++ 221121/강소현_programmers_과일 장수.java
@@ -1,26 +1,19 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        
-        List<Integer> list = new ArrayList<>();
-        int[] reverseScore = new int[score.length];
-        
+
         Arrays.sort(score);
         
-        for(int s : score) {
-            list.add(s);
-        }
-        
-        Collections.reverse(list);
-        
-        for(int i = 0; i < score.length; i++) {
-            reverseScore[i] = list.get(i);
-        }
-        
+        List<Integer> list = Arrays.stream(score).boxed()
+            .sorted(Comparator.reverseOrder())
+            .collect(Collectors.toList());
+    
+
         for(int i = 1; i < (score.length / m) + 1; i++) {
-            answer += reverseScore[m * i - 1] * m;
+            answer += list.get(m * i - 1) * m;
         }
         
         return answer;
